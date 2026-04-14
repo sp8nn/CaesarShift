e32ffa0bd214f094e50c282b60b167371451b962
sp8nn <noah.culley@student.centralia.edu>
2026-04-14 14:42:26 -0700

    Created unit tests and the CaesarShift stub class

--- CaesarShiftClass.cs
+++ CaesarShiftClass.cs
@@ -1,22 +1,37 @@
-﻿namespace CaesarShift
+﻿using System.Linq;
+
+namespace CaesarShift
 {
     public class CaesarShiftClass
     {
-        private int _key;
+        private readonly int _key;
 
         public CaesarShiftClass(int key)
         {
-            _key = key;
+            _key = key % 26;
         }
 
         public string Encrypt(string input)
         {
-            throw new NotImplementedException();
+            return Shift(input, _key);
         }
 
         public string Decrypt(string input)
         {
-            throw new NotImplementedException();
+            return Shift(input, -_key);
+        }
+
+        private string Shift(string text, int shift)
+        {
+            return new string(text.Select(c =>
+            {
+                if (!char.IsLetter(c)) return c;
+
+                char baseChar = char.IsUpper(c) ? 'A' : 'a';
+                int offset = c - baseChar;
+
+                return (char)(baseChar + (offset + shift + 26) % 26);
+            }).ToArray());
         }
     }
 }
