0ada97f94d76dbe683d497f2d0f52dc5115b8730
sp8nn <noah.culley@student.centralia.edu>
2026-04-14 14:39:49 -0700

    Add project files.

---
+++ CaesarShiftClass.cs
@@ -0,0 +1,22 @@
+﻿namespace CaesarShift
+{
+    public class CaesarShiftClass
+    {
+        private int _key;
+
+        public CaesarShiftClass(int key)
+        {
+            _key = key;
+        }
+
+        public string Encrypt(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Decrypt(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
